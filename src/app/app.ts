bfb967f92ac077c5fd5251e0d0e981548799de4a
Wesley DuSell <dusellfam@gmail.com>
2025-07-12 01:12:35 -0400

    Removing Routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'ngx-root',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [MatButtonModule],
   template: `
     <h1>Welcome to {{ title }} Testing Change!</h1>
     <a mat-flat-button >Testing</a>
-    <router-outlet />
   `,
   styles: [],
 })
