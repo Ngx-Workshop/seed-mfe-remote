eac18713e7ba9163c6278bc8bf0f5ec0dbfa41bc
Wesley DuSell <dusellfam@gmail.com>
2025-08-12 01:51:52 -0400

    shareAll is convenient but can bite you later.

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,56 @@ module.exports = withModuleFederationPlugin({
   },
 
   shared: {
-    ...shareAll({
+    '@angular/core': {
       singleton: true,
       strictVersion: true,
       requiredVersion: 'auto',
-    }),
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/forms': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+
+    // If you use Material/CDK, share them too
+    '@angular/cdk': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/material': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+
+    // RxJS + tslib
+    rxjs: {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    tslib: {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+
+    // Only if you’re NOT zoneless:
+    'zone.js': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
   },
 });
