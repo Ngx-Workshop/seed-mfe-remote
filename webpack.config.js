d33db56763b8c4ce7298870a8385179cdebcaa32
Wesley DuSell <dusellfam@gmail.com>
2025-10-03 22:48:26 -0400

    Updating tmdjr/ngx-user-metadata

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = withModuleFederationPlugin({
     '@tmdjr/ngx-user-metadata': {
       singleton: true,
       strictVersion: true,
-      requiredVersion: '^0.0.6',
+      requiredVersion: '^0.0.7',
     },
   },
 });
